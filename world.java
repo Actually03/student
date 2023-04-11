2ff6c3619a528340672b17dad47ce0886b25fe51
yyds <1973418542@qq.com>
2023-04-11 10:25:35 +0800

    在hot_fix分支添加Student.java

---
+++ world.java
@@ -0,0 +1,7 @@
+public class world{
+	public static void main(String[] args){
+		System.out.println("world line 1")
+		System.out.println("world line 2")
+		System.out.println("world line 3")
+	}
+}
