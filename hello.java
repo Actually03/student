226e787341627b5fae7f6d0c29902c8233a40bbc
yyds <1973418542@qq.com>
2023-04-11 08:55:02 +0800

    初始提交

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello{
+	public static void main(String[] args){
+		System.out.println("hello world line 1");
+		System.out.println("hello world line 2");
+		System.out.println("hello world line 3");
+		System.out.println("hello world line 4");
+		System.out.println("hello world line 5");
+	}
+}
